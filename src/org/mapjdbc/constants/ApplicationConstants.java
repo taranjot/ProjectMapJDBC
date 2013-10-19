fbf61f7ffdb1755ca8b4ad5f56904736aec04b2a
taranjot <jotwanitaran@gmai.com>
2013-10-19 14:06:12 -0700

    Adding an Exception and constant file

---
+++ src/org/mapjdbc/constants/ApplicationConstants.java
@@ -0,0 +1,10 @@
+package org.mapjdbc.constants;
+
+/**
+ * This file is used as a constant handler.
+ * */
+public class ApplicationConstants {
+	
+	public static final String ERROR_MESSAGE = "The application has failed to initialize. Please contact the Administrator";
+
+}
---
+++ src/org/mapjdbc/exceptions/ApplicationException.java
@@ -0,0 +1,47 @@
+package org.mapjdbc.exceptions;
+
+import org.mapjdbc.constants.ApplicationConstants;
+
+
+/**
+ * The Application Exception is raised when an application has to stop functioning due to some error.
+ * Please check and fill the error code when catching any kind of exception.
+ * 
+ * @author Taran
+ * 
+ * */
+public class ApplicationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Integer errCode;
+	private String errorMessage;
+	
+	/**
+	 * Throw an default exception only in case of errors.
+	 * */
+	public ApplicationException() {
+		this.errCode = 99;
+		this.errorMessage = ApplicationConstants.ERROR_MESSAGE;
+	}
+	
+	/**
+	 * @param errCode
+	 * @param errorMessage
+	 */
+	
+	public ApplicationException(Integer errCode, String errorMessage) {
+		super();
+		this.errCode = errCode;
+		this.errorMessage = errorMessage;
+	}
+	
+	
+	
+	
+
+}
